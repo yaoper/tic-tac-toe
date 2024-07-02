386e0bddd1b619212ee9200811ae6bfa9d48b204
yaoper <yaoper@163.com>
2024-07-02 17:48:21 +0800

    add set x method

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
 import { useState } from "react";
 
-function Square(){
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    console.log('Clicked!')
-    setValue('X')
-  }
-
-  return <button className="square" onClick={handleClick}>{value}</button>;
+function Square({value, onSquareClick}){
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(){
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[0]} onSquareClick={handleClick}/>
+        <Square value={squares[1]} onSquareClick={handleClick}/>
+        <Square value={squares[2]} onSquareClick={handleClick}/>
       </div>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[3]} onSquareClick={handleClick}/>
+        <Square value={squares[4]} onSquareClick={handleClick}/>
+        <Square value={squares[5]} onSquareClick={handleClick}/>
       </div>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[6]} onSquareClick={handleClick}/>
+        <Square value={squares[7]} onSquareClick={handleClick}/>
+        <Square value={squares[8]} onSquareClick={handleClick}/>
       </div>
     </>
   );
