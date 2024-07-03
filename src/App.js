3bd1e6a284d77f5fd0abebd153c7475930eff795
yaoper <yaoper@163.com>
2024-07-03 21:43:48 +0800

    add set o method

--- src/App.js
+++ src/App.js
@@ -6,11 +6,17 @@ function Square({value, onSquareClick}){
 
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i){
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setXIsNext(!xIsNext);
     setSquares(nextSquares);
   }
 
