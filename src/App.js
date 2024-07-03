c6d2db311dbece23abbb7556c43be5dc6bf964f6
yaoper <yaoper@163.com>
2024-07-03 22:43:15 +0800

    add commpent game

--- src/App.js
+++ src/App.js
@@ -8,11 +8,7 @@ function Square({value, onSquareClick}){
   );
 }
 
-
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board(xIsNext, squares, onPlay) {
   function handleClick(i){
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -23,8 +19,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setXIsNext(!xIsNext);
-    setSquares(nextSquares);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -77,4 +72,30 @@ function calculateWinner(squares) {
     }
   }
   return null;
+}
+
+
+
+export default function Game(){
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+
+  );
+
 }
